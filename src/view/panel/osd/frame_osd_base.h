403b3961733a78438fedefbaedf062e3673f934e
miftah <salam.miftah@gmail.com>
2024-06-27 09:28:34 +0700

    adjust - qt linting

--- src/view/panel/osd/frame_osd_base.h
+++ src/view/panel/osd/frame_osd_base.h
@@ -6,37 +6,36 @@
 #include "src/model/osd/input_mode_model.h"
 #include "src/view/shared/constant_ui.h"
 
-template <typename DataStreamModel, typename DataResponseModel>
-class FrameOSDBase
+template <typename DataStreamModel, typename DataResponseModel> class FrameOSDBase
 {
 public:
-    virtual void setup() = 0;          // ui first initialization
-    virtual void resetModeIndex() = 0; // handler when set mode request failed
+    virtual void setup() = 0; //ui first initialization
+    virtual void resetModeIndex() = 0; //handler when set mode request failed
 
 public slots:
-    virtual void onModeChangeResponse(InputModeModel mode) = 0; // handler set mode response from server
-    virtual void onStreamReceive(DataStreamModel model) = 0;    // handler data stream input
-    virtual void onDataResponse(DataResponseModel data) = 0;    // handler manual data response from server
+    virtual void onModeChangeResponse(InputModeModel mode) = 0; //handler set mode response from server
+    virtual void onStreamReceive(DataStreamModel model) = 0; //handler data stream input
+    virtual void onDataResponse(DataResponseModel data) = 0; //handler manual data response from server
 
 protected:
-    QTimer *timer; // timer to update ui
+    QTimer *timer; //timer to update ui
     QDateTime timestamp;
-    OSD_MODE currentMode;    // hold current data fisis mode
-    int currentModeIndx;     // hold current data fisis mode combobox index
-    bool afterResetModeIndx; // flag for toggling set mode reset handler
+    OSD_MODE currentMode;  //hold current data fisis mode
+    int currentModeIndx; //hold current data fisis mode combobox index
+    bool afterResetModeIndx; //flag for toggling set mode reset handler
 
 private slots:
-    virtual void onModeChange(int index) = 0; // handler ui combobox mode changed event
-    virtual void onAfterModeReset() = 0;      // handler when set mode request failed event
+    virtual void onModeChange(int index) = 0; //handler ui combobox mode changed event
+    virtual void onAfterModeReset() = 0;  //handler when set mode request failed event
     virtual void onTimeout() = 0;
 
 private:
-    virtual void manualUiSetup() = 0;       // ui manual mode method
-    virtual void autoUiSetup() = 0;         // ui auto mode method
-    virtual void notConnectedUiSetup() = 0; // ui not connected to data stream method
-    virtual void noDataUiSetup() = 0;       // ui no data from stream method
-    virtual void invalidDataUiSetup() = 0;  // ui invalid data from stream method
-    virtual bool validateInput() = 0;       // ui validate data manual user input method
+    virtual void manualUiSetup() = 0; //ui manual mode method
+    virtual void autoUiSetup() = 0; //ui auto mode method
+    virtual void notConnectedUiSetup() = 0; //ui not connected to data stream method
+    virtual void noDataUiSetup() = 0; //ui no data from stream method
+    virtual void invalidDataUiSetup() = 0; //ui invalid data from stream method
+    virtual bool validateInput() = 0; //ui validate data manual user input method
 };
 
 #endif // FRAME_OSD_BASE_H
