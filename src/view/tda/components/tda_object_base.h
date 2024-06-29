18398db0ccb3c2a6d5567ac6e36fc03edd8f9ad7
miftah <salam.miftah@gmail.com>
2024-06-29 10:22:13 +0700

    enhance - create tda zoomable object base class

--- src/view/tda/components/tda_object_base.h
+++ src/view/tda/components/tda_object_base.h
@@ -13,4 +13,17 @@ public:
     virtual void Draw(QPainter* painter, const int &side, const int &width, const int &height, const QPoint &off_center) = 0;
 };
 
+class TDAZoomableObjectBase : public TDAObjectBase
+{
+    Q_OBJECT
+public:
+    TDAZoomableObjectBase(QObject* parent);
+
+public slots:
+    virtual void OnZoom(float range) = 0;
+
+private:
+    float currentRange;
+};
+
 #endif // TDA_OBJECT_BASE_H
