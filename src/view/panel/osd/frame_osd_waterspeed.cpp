98a9ae854007cdf925ae31fff39df444b02936e5
buya07 <riyadhi.aptrg@gmail.com>
2024-07-11 16:06:27 +0700

    add validasi input text

--- src/view/panel/osd/frame_osd_waterspeed.cpp
+++ src/view/panel/osd/frame_osd_waterspeed.cpp
@@ -264,19 +264,20 @@ void FrameOSDWaterSpeed::on_pushButton_clicked()
 
 bool FrameOSDWaterSpeed::validateInput()
 {
+    bool ok1,ok2;
     QString wSpeed = ui->inputWaterSpeed->getCurrentValue();
-    float value_wSpeed = wSpeed.toFloat();
+    float value_wSpeed = wSpeed.toFloat(&ok1);
 
-    if ((value_wSpeed < -150) || (value_wSpeed > 150))
+    if ((value_wSpeed < -150) || (value_wSpeed > 150) || (!ok1))
     {
         QMessageBox::critical(this, "Fatal Error Water Speed", "Invalid input : out of range.\nValid input : -150 to 150");
         return false;
     }
 
     QString wCourse = ui->inputWaterCourse->getCurrentValue();
-    float value_wCourse = wCourse.toFloat();
+    float value_wCourse = wCourse.toFloat(&ok2);
 
-    if ((value_wCourse < 0) || (value_wCourse > 360))
+    if ((value_wCourse < 0) || (value_wCourse > 360) || (!ok2))
     {
         QMessageBox::critical(this, "Fatal Error Water Course", "Invalid input : out of range.\nValid input : 0 to 360");
         return false;
