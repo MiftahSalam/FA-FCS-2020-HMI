9a2ef2aeda04f43b669f97f81dc1900f2c6b388b
buya07 <riyadhi.aptrg@gmail.com>
2024-07-22 21:42:34 +0700

    add repo inertia heading

--- src/view/tda/components/tda_heading_marker_object.cpp
+++ src/view/tda/components/tda_heading_marker_object.cpp
@@ -1,29 +1,40 @@
 #include "tda_heading_marker_object.h"
+#include "src/di/di.h"
 
-TDAHeadingMarkerObject::TDAHeadingMarkerObject(QObject *parent): TDAObjectBase (parent){}
+#include "qmath.h"
+#include <QTextStream>
+#include <cmath>
+
+TDAHeadingMarkerObject::TDAHeadingMarkerObject(QObject *parent): TDAObjectBase (parent)
+{
+    osdRepo = DI::getInstance()->getRepoOSD();
+}
 
 void TDAHeadingMarkerObject::Draw(QPainter *painter, const int &side, const int &width, const int &height, const QPoint &off_center)
 {
     QPoint center_point = QPoint(width/2,height/2);
+    int sideMax = qMax(width,height);
 
     const bool show_headingMarker = true;
-    // const double heading = 90;
 
     if (show_headingMarker)
     {
         painter->translate(center_point);
 
-        const double heading = 300;
-
+        auto inertia = osdRepo->getRepoOSDInertia()->GetInertia();
+        const double heading = inertia->heading();
+        // const double heading = 30;
         double drawHeading = heading + 180;
+
         if (drawHeading > 180)
             drawHeading = drawHeading - 360;
-        // int sideMax = qMax (width(),height());
-        // painter->rotate(-static_cast<qreal>(heading));
-        // painter->translate(center_point);
         painter->rotate(drawHeading);
-        painter->setPen(QColor(255,255,0,255));
-        painter->drawLine(0,0,0, 1000);
-
+        QPen linePen(QColor(255,255,0,255));
+        linePen.setWidth(0);
+        painter->setPen(linePen);
+        painter->drawLine(0,0,0, sideMax);
+        painter->rotate(-drawHeading);
     }
 }
+
+
--- src/view/tda/components/tda_heading_marker_object.h
+++ src/view/tda/components/tda_heading_marker_object.h
@@ -4,13 +4,18 @@
 #include <QObject>
 
 #include "tda_object_base.h"
+#include "src/infra/store/osd/osd_repository.h"
 
 class TDAHeadingMarkerObject: public TDAObjectBase
 {
     Q_OBJECT
 public:
     TDAHeadingMarkerObject(QObject* parent = nullptr);
+
     void Draw(QPainter *painter, const int &side, const int &width, const int &height, const QPoint &off_center) override;
+
+private:
+    OSDRepository *osdRepo;
 };
 
 #endif // TDA_HEADING_MARKER_OBJECT_H
