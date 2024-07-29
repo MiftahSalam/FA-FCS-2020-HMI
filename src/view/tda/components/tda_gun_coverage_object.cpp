6a5838318a32828035d3e198ffdc448d012f8dea
buya07 <riyadhi.aptrg@gmail.com>
2024-07-29 15:11:41 +0700

    reposisi centre gun coverage in tda frame

--- src/view/tda/components/tda_gun_coverage_object.cpp
+++ src/view/tda/components/tda_gun_coverage_object.cpp
@@ -20,10 +20,10 @@ void TDAGunCoverageObject::Draw(QPainter *painter, const int &side, const int &w
         // const qreal blind_arc = QString(blind_arc1).toDouble();
         // const qreal max_range = QString(max_range1).toDouble(); //NM
 
-        double gun_orientation = 90;
-        double blind_arc = 45;
+        double gun_orientation = 0; //posisi gun haluan
+        double blind_arc = 90;
         // double max_range = 2;
-        double currHeading = 100;
+        double currHeading = 45;
 
         int span = 360-blind_arc;
         int gun_coveragePixel = 600;
@@ -31,9 +31,10 @@ void TDAGunCoverageObject::Draw(QPainter *painter, const int &side, const int &w
 
         painter->translate(center_point);
         painter->setPen(QColor(255,0,0,255));
-        painter->rotate(-gun_orientation-currHeading-qreal((span/2)-90));
+        painter->rotate(gun_orientation+currHeading+qreal((span/2)-90));
         painter->drawPie(-gun_coveragePixel/2,-gun_coveragePixel/2,gun_coveragePixel,
                          gun_coveragePixel,0*16,span*16);
+        painter->rotate(-gun_orientation-currHeading-qreal((span/2)-90));
 
         QRect rect(-((gun_coveragePixel/2)+20)*cos((currHeading+100)*M_PI/180)-15,-((gun_coveragePixel/2)+20)*sin((currHeading+100)*M_PI/180)-5,30,15);
         QTextOption opt;
@@ -44,5 +45,6 @@ void TDAGunCoverageObject::Draw(QPainter *painter, const int &side, const int &w
         painter->setFont(font);
         painter->drawText(rect,QString("40mm"),opt);
 
+        painter->translate(-center_point);
     }
 }
