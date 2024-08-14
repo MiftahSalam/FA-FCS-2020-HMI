4a19fbc6a26cc6bb2273c7fcede8bde96e1a34e9
miftah <salam.miftah@gmail.com>
2024-08-14 19:32:49 +0700

    enhance - prepare ui gun barrel control

--- src/view/panel/gun/frame_gun_control_barrel.cpp
+++ src/view/panel/gun/frame_gun_control_barrel.cpp
@@ -12,3 +12,52 @@ FrameGunControlBarrel::~FrameGunControlBarrel()
 {
     delete ui;
 }
+
+void FrameGunControlBarrel::setup()
+{
+    BarrelControlProp prop{
+        "Gun Barrel Control",
+        TextInputProp{
+            "Azimuth:", "deg", "AzimuthInput", "0"},
+        TextInputProp{
+            "Elevation:", "deg", "ElvationInput", "0"}};
+
+    ui->groupBoxGunBarrCon->setTitle(prop.title);
+    ui->inputAzimuth->setup(prop.azimuth);
+    ui->inputElevation->setup(prop.elevation);
+}
+
+void FrameGunControlBarrel::onDataResponse(BaseResponse<GyroModel> data)
+{
+
+}
+
+void FrameGunControlBarrel::onModeChange(int index)
+{
+
+}
+
+void FrameGunControlBarrel::onTimeout()
+{
+
+}
+
+void FrameGunControlBarrel::on_pushButton_clicked()
+{
+
+}
+
+void FrameGunControlBarrel::manualUiSetup()
+{
+
+}
+
+void FrameGunControlBarrel::autoUiSetup()
+{
+
+}
+
+bool FrameGunControlBarrel::validateInput()
+{
+
+}
--- src/view/panel/gun/frame_gun_control_barrel.h
+++ src/view/panel/gun/frame_gun_control_barrel.h
@@ -1,11 +1,19 @@
 #ifndef FRAME_GUN_CONTROL_BARREL_H
 #define FRAME_GUN_CONTROL_BARREL_H
 
+#include "src/view/shared/frame_text_input.h"
 #include <QFrame>
 
 namespace Ui {
 class FrameGunControlBarrel;
 }
+
+struct BarrelControlProp
+{
+    QString title;
+    TextInputProp azimuth;
+    TextInputProp elevation;
+};
 
 class FrameGunControlBarrel : public QFrame
 {
@@ -15,8 +23,29 @@ public:
     explicit FrameGunControlBarrel(QWidget *parent = nullptr);
     ~FrameGunControlBarrel();
 
+    void setup();
+
+public slots:
+//    void onModeChangeResponse(const QString datafisis, BaseResponse<InputModeModel> mode, bool needConfirm);
+    void onDataResponse(BaseResponse<GyroModel> data);
+
+signals:
+    void signalChangeControlMode(bool manual_mode);
+    void signalChangeBarrelData(float azimuth, float elevation);
+
+private slots:
+    void onModeChange(int index);
+    void onTimeout();
+
+    void on_pushButton_clicked();
+
 private:
     Ui::FrameGunControlBarrel *ui;
+
+    void manualUiSetup();
+    void autoUiSetup();
+    bool validateInput();
+
 };
 
 #endif // FRAME_GUN_CONTROL_BARREL_H
