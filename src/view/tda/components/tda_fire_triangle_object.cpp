5b010817e35b20066f982ccba98ca75669874990
buya07 <riyadhi.aptrg@gmail.com>
2024-08-29 19:46:42 +0700

    draw tda firetriangle

--- src/view/tda/components/tda_fire_triangle_object.cpp
+++ src/view/tda/components/tda_fire_triangle_object.cpp
@@ -6,7 +6,7 @@
 TDAFireTriangleObject::TDAFireTriangleObject(QObject *parent, FireTriangleBaseRepository *repoFireTriangle):
     TDAObjectBase (parent), fireTriangleRepo(repoFireTriangle)
 {
-    fireTriangleRepo->SetFireTriangle(FireTriangleEntity(60,100,200));
+    fireTriangleRepo->SetFireTriangle(FireTriangleEntity(60,10,20));
 }
 
 void TDAFireTriangleObject::Draw(QPainter *painter, const int &side, const int &width, const int &height, const QPoint &off_center)
@@ -18,9 +18,16 @@ void TDAFireTriangleObject::Draw(QPainter *painter, const int &side, const int &
 
     //posisi track
     float track_x = 50;
-    float track_y = 70;
+    float track_y = 100;
+
+    QPoint points[3] = {
+        center_point,
+        QPoint(ttlf_x,ttlf_y),
+        QPoint(track_x, track_y)
+    };
 
     painter->translate(center_point);
     painter->setPen(QColor(0,0,255,255));
-
+    painter->drawPolygon(points, 3);
+    painter->translate(-center_point);
 }
