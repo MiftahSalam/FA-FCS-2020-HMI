c85bcd2363b17e44bff597e432f5dbf2fcba9991
buya07 <riyadhi.aptrg@gmail.com>
2024-08-30 18:22:13 +0700

    update scale draw triangle

--- src/view/tda/components/tda_fire_triangle_object.cpp
+++ src/view/tda/components/tda_fire_triangle_object.cpp
@@ -7,7 +7,7 @@
 TDAFireTriangleObject::TDAFireTriangleObject(QObject *parent, FireTriangleBaseRepository *repoFireTriangle, TDAConfig *configTDA):
     TDAObjectBase (parent), fireTriangleRepo(repoFireTriangle), tdaConfig(configTDA)
 {
-    fireTriangleRepo->SetFireTriangle(FireTriangleEntity(60,2000,3000));
+    fireTriangleRepo->SetFireTriangle(FireTriangleEntity(60,2000,2000));
 }
 
 void TDAFireTriangleObject::Draw(QPainter *painter, const int &side, const int &width, const int &height, const QPoint &off_center)
@@ -21,27 +21,29 @@ void TDAFireTriangleObject::Draw(QPainter *painter, const int &side, const int &
     float ttlf_x_NM = ttlf_x/1852;
     float ttlf_y_NM = ttlf_y/1852;
 
-    int ttlf_x_Pixel = (int)(ttlf_x_NM*(width/2*tdaScale));
-    int ttlf_y_Pixel = (int)(ttlf_y_NM*(width/2*tdaScale));
+    int ttlf_x_Pixel = (int)(ttlf_x_NM*(side/tdaScale));
+    int ttlf_y_Pixel = (int)(ttlf_y_NM*(side/tdaScale));
 
+    qDebug()<< "side-------------"<<side;
     qDebug()<< "----------------ttlf x"<<ttlf_x_Pixel;
     qDebug()<< "----------------ttlf y"<<ttlf_y_Pixel;
 
     //posisi track
-    float track_x = 50;
-    float track_y = 100;
+    float track_x = 5000;
+    float track_y = 2000;
 
+    //konversi ke NM
+    float track_x_NM = track_x/1852;
+    float track_y_NM = track_y/1852;
 
+    int track_x_Pixel = (int)(track_x_NM*(side/tdaScale));
+    int track_y_Pixel = (int)(track_y_NM*(side/tdaScale));
 
-    QPoint points[3] = {
-        center_point,
-        QPoint(ttlf_x_Pixel,ttlf_y_Pixel), // ttlf
-        QPoint(width/2+50, height/2+150) // tracks
-    };
-
-    // painter->translate(center_point);
-    painter->setPen(QColor(0,0,255,255));
-    painter->drawPolygon(points, 3);
-    // painter->translate(-center_point);
+    painter->translate(center_point);
+    painter->setPen(QColor(30,144,255,255));
+    painter->drawLine(0,0, ttlf_x_Pixel, ttlf_y_Pixel);
+    painter->drawLine(ttlf_x_Pixel, ttlf_y_Pixel, track_x_Pixel, track_y_Pixel);
+    painter->drawLine(track_x_Pixel, track_y_Pixel, 0, 0);
+    painter->translate(-center_point);
 }
 
