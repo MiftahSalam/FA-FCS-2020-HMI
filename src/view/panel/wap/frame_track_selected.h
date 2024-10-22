6f0d784417df315c6d095c5fb380cdbcb41045d9
miftah <salam.miftah@gmail.com>
2024-10-22 10:46:59 +0700

    enhance - implement track selected display handler

--- src/view/panel/wap/frame_track_selected.h
+++ src/view/panel/wap/frame_track_selected.h
@@ -1,13 +1,15 @@
 #ifndef FRAME_TRACK_SELECTED_H
 #define FRAME_TRACK_SELECTED_H
 
+#include "src/domain/track/repository/track_base_repository.h"
+#include "src/infra/store/track/track_repository_listener.h"
 #include <QWidget>
 
 namespace Ui {
 class FrameTrackSelected;
 }
 
-class FrameTrackSelected : public QWidget
+class FrameTrackSelected : public QWidget, public TrackRepositoryListener
 {
     Q_OBJECT
 
@@ -15,8 +17,21 @@ public:
     explicit FrameTrackSelected(QWidget *parent = nullptr);
     ~FrameTrackSelected();
 
+    // TrackRepositoryListener interface
+    void OnTracksAdded(std::list<TrackBaseEntity *> tnList) override;
+    void OnTracksRemoved(std::list<int> tnIdList) override;
+    void OnTrackPropertyChanged(int tn, TrackBaseEntity *track) override;
+    void OnTrackSelectedChanged(int tn) override;
+
+private slots:
+    void on_pushButtonClear_clicked();
+
 private:
     Ui::FrameTrackSelected *ui;
+    TrackBaseRepository *arpaRepo;
+
+    int currentTrackSelected;
+
 };
 
 #endif // FRAME_TRACK_SELECTED_H
